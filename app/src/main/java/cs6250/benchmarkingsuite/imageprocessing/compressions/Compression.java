dde5caf752af9564db2c02d2de5b1cc6c70fe787
R. Elliott Childre <rchildre3@gatech.edu>
2017-11-02 18:51:39 -0400

    init Compression UI

---
+++ app/src/main/java/cs6250/benchmarkingsuite/imageprocessing/compressions/Compression.java
@@ -0,0 +1,10 @@
+package cs6250.benchmarkingsuite.imageprocessing.compressions;
+
+import java.io.Serializable;
+
+/**
+ * Created by Elliott on 11/2/2017.
+ */
+
+public abstract class Compression implements Serializable {
+}
