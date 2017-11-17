07e42099c5169cdc549d6a8da4d41a902abaa4ab
gtg986s <gtg986s@gatech.edu>
2017-11-17 12:19:19 -0500

    Remove dlib from grayscale

--- app/src/main/java/cs6250/benchmarkingsuite/imageprocessing/effects/GrayscaleEffect.java
+++ app/src/main/java/cs6250/benchmarkingsuite/imageprocessing/effects/GrayscaleEffect.java
@@ -1,25 +1,7 @@
 package cs6250.benchmarkingsuite.imageprocessing.effects;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Point;
-import android.util.Log;
-
-import org.opencv.android.Utils;
-import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import com.tzutalin.dlib.Constants;
-import com.tzutalin.dlib.FaceDet;
-import com.tzutalin.dlib.VisionDetRet;
 
 public class GrayscaleEffect extends Effect {
 
